84f1b1ba7d821d30ca89151f972eecefa6900d43
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-02-13 14:35:53 +0000

    Applied switch statements at App.js

--- my-drum-machine/src/App.js
+++ my-drum-machine/src/App.js
@@ -28,15 +28,15 @@ Taken from Steven Youhana - Originally Accessed 12 February 2020
 // Define Constant
 
 const SOUNDS = [
-  {Q: 'Drum-Roll-SoundBible.com-1599301580.mp3'},
-  {W: 'Halogen Light-SoundBible.com-1664817471.mp3'},
-  {E: 'Sci-Fi-Robot-SoundBible.com-481033379.mp3'},
-  {A: 'City_Centre-Hopeinawe-377331566.mp3'},
-  {S: 'my-drum-machine/src/service-bell_daniel_simion.mp3'},
-  {D: 'Pen-Clicking-SoundBible.com-482574858.mp3'},
-  {Z: 'Public-Transit-Bus-SoundBible.com-671541921.mp3'},
-  {X: 'party_horn-Mike_Koenig-76599891.mp3'},
-  {C: 'Typewriter-SoundBible.com-285954942.mp3'}
+  { Q: 'Drum-Roll-SoundBible.com-1599301580.mp3' },
+  { W: 'Halogen Light-SoundBible.com-1664817471.mp3' },
+  { E: 'Sci-Fi-Robot-SoundBible.com-481033379.mp3' },
+  { A: 'City_Centre-Hopeinawe-377331566.mp3' },
+  { S: 'my-drum-machine/src/service-bell_daniel_simion.mp3' },
+  { D: 'Pen-Clicking-SoundBible.com-482574858.mp3' },
+  { Z: 'Public-Transit-Bus-SoundBible.com-671541921.mp3' },
+  { X: 'party_horn-Mike_Koenig-76599891.mp3' },
+  { C: 'Typewriter-SoundBible.com-285954942.mp3' }
 ];
 
 // Set Up Components
@@ -49,20 +49,61 @@ class DrumMachine extends React.Component {
       padColor: 'lavender',
       displayColor: 'lightgreen'
     };
-    
+
     this.toPlay = null;
     this.playAudio = this.playAudio.bind(this);
     this.setDisplay = this.setDisplay.bind(this);
     this.build = null;
-    
-    
-    
-    
-    
+
   }
+
+  playAudio(btn) {
+    if (btn.target.id >= 0 && btn.target.id <= 8) {
+      this.toPlay = document.getElementById(Object.keys(SOUNDS[btn.target.id]));
+      this.toPlay.currentTime = 0;
+      for (const e in SOUNDS) {
+        if (e === btn.target.id) this.url = SOUNDS[btn.target.id];
+        this.toPlay.play();
+      }
+    }
+    this.setDisplay(parseInt(btn.target.id, 10));
+  }
+  
+   setDisplay(hit) {
+    if(hit >= 0 && hit <= 8) {
+      let rgb = [Math.floor(Math.random()*254),Math.floor(Math.random()*254),Math.floor(Math.random()*254)];
+      document.getElementById("display").style.background = `rgb(${rgb[1]},${rgb[0]},${rgb[2]})`;
+      document.getElementById("view").style.background = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+      document.getElementById(hit).style.background = 'white';
+    }
+   setTimeout(()=> {
+        document.getElementById(hit).style.background = 'lavender';
+      },200);
+    
+    let setIt = (theHit) => {
+      // console.log(theHit);
+      this.setState({
+        display: theHit
+      });
+    };
+    
+    switch(hit) {
+      case 0: setIt('Kick'); break;
+      case 1: setIt('That snare!'); break;
+      case 2: setIt('hat El'); break;
+      case 3: setIt('hat'); break;
+      case 4: setIt('Impact hit'); break;
+      case 5: setIt('Rim'); break;
+      case 6: setIt('Funk 1'); break;
+      case 7: setIt('Funk 2'); break;
+      case 8: setIt('Funk 3'); break;
+      default: setIt('You Rock!'); break;
+    }
+  }
+
   render() {
-  return (
-    <div className="App">
+    return (
+      <div className="App">
       <header className="App-header">
         
         <p>
@@ -78,7 +119,7 @@ class DrumMachine extends React.Component {
         </a>
       </header>
     </div>
-  );
-}
+    );
+  }
 }
 export default DrumMachine;
