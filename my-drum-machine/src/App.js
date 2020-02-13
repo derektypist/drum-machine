c5f2a3ed3d742b77609cf7a5e1b4dfb15b9daddf
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-02-13 14:43:41 +0000

    Added handleKeys at App.js

--- my-drum-machine/src/App.js
+++ my-drum-machine/src/App.js
@@ -11,16 +11,6 @@ SOURCES
 ‘http://soundbible.com/835-Pen-Clicking.html’
 ‘http://soundbible.com/1698-Public-Transit-Bus.html’
 ‘http://soundbible.com/1817-Party-Horn.html’
-‘https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3 25’
-‘https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3 37’
-‘https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3 17’
-‘https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3 29’
-‘https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3 28’
-‘https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3 17’
-‘https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3 33’
-‘https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3 40’
-‘https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3 35’
-‘https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3 46’
 
 Taken from Steven Youhana - Originally Accessed 12 February 2020
 */
@@ -32,7 +22,7 @@ const SOUNDS = [
   { W: 'Halogen Light-SoundBible.com-1664817471.mp3' },
   { E: 'Sci-Fi-Robot-SoundBible.com-481033379.mp3' },
   { A: 'City_Centre-Hopeinawe-377331566.mp3' },
-  { S: 'my-drum-machine/src/service-bell_daniel_simion.mp3' },
+  { S: 'service-bell_daniel_simion.mp3' },
   { D: 'Pen-Clicking-SoundBible.com-482574858.mp3' },
   { Z: 'Public-Transit-Bus-SoundBible.com-671541921.mp3' },
   { X: 'party_horn-Mike_Koenig-76599891.mp3' },
@@ -88,18 +78,71 @@ class DrumMachine extends React.Component {
     };
     
     switch(hit) {
-      case 0: setIt('Kick'); break;
-      case 1: setIt('That snare!'); break;
-      case 2: setIt('hat El'); break;
-      case 3: setIt('hat'); break;
-      case 4: setIt('Impact hit'); break;
-      case 5: setIt('Rim'); break;
-      case 6: setIt('Funk 1'); break;
-      case 7: setIt('Funk 2'); break;
-      case 8: setIt('Funk 3'); break;
+      case 0: setIt('Drum Roll'); break;
+      case 1: setIt('Halogen Light'); break;
+      case 2: setIt('Sci Fi Robot'); break;
+      case 3: setIt('City Centre'); break;
+      case 4: setIt('Service Bell'); break;
+      case 5: setIt('Pen Clicking'); break;
+      case 6: setIt('Public Transit Bus'); break;
+      case 7: setIt('Party Horn'); break;
+      case 8: setIt('Typewriter'); break;
       default: setIt('You Rock!'); break;
     }
   }
+
+handleKey = (event) => {
+    switch(event) {
+      case SOUNDS[0]: 
+        this.toPlay = document.getElementById('Q');        
+        this.toPlay.play(); 
+        this.setDisplay(0);
+        break;
+      case SOUNDS[1]: 
+        this.toPlay = document.getElementById('W');
+        this.toPlay.play();
+        this.setDisplay(1);
+        break;
+      case SOUNDS[2]: 
+        this.toPlay = document.getElementById('E');
+        this.toPlay.play();
+        this.setDisplay(2);
+        break;
+      case SOUNDS[3]: 
+        this.toPlay = document.getElementById('A');
+        this.toPlay.play(); 
+        this.setDisplay(3);
+        break;
+      case SOUNDS[4]: 
+        this.toPlay = document.getElementById('S');
+        this.toPlay.play();
+        this.setDisplay(4);
+        break;
+      case SOUNDS[5]: 
+        this.toPlay = document.getElementById('D'); 
+        this.toPlay.play();
+        this.setDisplay(5);
+        break;
+      case SOUNDS[6]: 
+        this.toPlay = document.getElementById('Z'); 
+        this.toPlay.play();
+        this.setDisplay(6);
+        break;
+      case SOUNDS[7]: 
+        this.toPlay = document.getElementById('X');
+        this.toPlay.play(); 
+        this.setDisplay(7);
+        break;
+      case SOUNDS[8]: 
+        this.toPlay = document.getElementById('C'); 
+        this.toPlay.play(); 
+        this.setDisplay(8);
+        break;
+    }
+    
+  }
+
+
 
   render() {
     return (
